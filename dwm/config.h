c3161fd6b681308c7a4b4a7377e35c88be66cb8b
Italo dos Santos Ferreira <iferreira@iff.edu.br>
2023-05-16 16:19:37 -0300

    Adicionando dwm; rofi; slock; xmenu; vimrc

---
+++ dwm/config.h
@@ -0,0 +1,165 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+
+/* key definitions */
+/* Mod1Mask e Alt
+ * Mod4Mask e Super_L
+ */
+#define MODKEY Mod1Mask
+#define MODKEY2 Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "xterm", NULL };
+static const char *midcom[]  = { "xterm", "-e", "mc", NULL };
+/*static const char *rofimenupower[]  = { "rofi", "-show", "p", "-modi", "p:/home/italo/Workspace/dwm_all/rofi-power-menu/rofi-power-menu", "-theme", "Paper", "-width", "20", "-lines", "12", NULL };
+static const char *rofimenu[]  = { "rofi", "-show", "combi", "-combi-modi", "window,drun,ssh", "-modi", "combi", NULL }; */
+static const char *gvim[]  = { "gvim", NULL };
+static const char *chrome[]  = { "google-chrome", NULL };
+static const char *slock[]  = { "slock", NULL };
+static const char *xdotoolfechar[]  = { "xdotool", "key", "alt+F4", NULL };
+static const char *xmenu_main[]  = { "/home/italo/Workspace/dwm_all/xmenu/xmenu_main.sh", NULL };
+static const char *xmenu_power[]  = { "/home/italo/Workspace/dwm_all/xmenu/xmenu_power.sh", NULL };
+/*static const char *surf[] = { "surf", "duckduckgo.com", NULL };*/
+
+static const Launcher launchers[] = {
+  /* command       name to display */
+	{ chrome,         "Go"   },
+	{ termcmd,        "Xt"   },
+	{ gvim,           "Vim"  },
+	{ xmenu_main,     "[M]"   },
+	{ xdotoolfechar,  "[X]"  },
+};
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* Atalhos Italo */
+	{ MODKEY2,                      XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY2,                      XK_v,      spawn,          {.v = gvim } },
+	{ MODKEY2,                      XK_c,      spawn,          {.v = chrome } },
+	{ MODKEY2,                      XK_e,      spawn,          {.v = midcom } },
+	{ MODKEY2,                      XK_l,      spawn,          {.v = slock } },
+	{ MODKEY2,                      XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = slock } },
+  { MODKEY|ControlMask,           XK_Right,  view_adjacent,  {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_Left,   view_adjacent,  {.i = -1 } },
+	{ MODKEY,                       XK_F4,     killclient,     {0} },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = xmenu_main } },
+	/*{0                    , 0x1008ff11 , spawn          , SHCMD ("amixer sset Master 2%-")},*/ /* Volume - */
+	/*{0                    , 0x1008ff12 , spawn          , SHCMD ("amixer sset Master toggle")},*/ /* Volume Mute */
+	/*{0                    , 0x1008ff13 , spawn          , SHCMD ("amixer sset Master 2%+")},*/ /* Volume + */
+	/*{0                    , 0x1008ffb2 , spawn          , SHCMD ("amixer sset Capture toggle")},*/ /* Mic Mute */
+	/*{0                    , 0x1008ff59 , spawn          , SHCMD ("$HOME/.xrandr-toggle")},*/ /* Monitores */
+
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  /* Atalhos Italo */
+	{ ClkStatusText,        0,              Button1,        spawn,          {.v = xmenu_power } },
+
+};
+
---
+++ vimrc
@@ -0,0 +1,176 @@
+" Tecla leader e atalhos
+" Trocando a tecla: 'let mapleader = ","'
+
+" Instalando pathogen
+" Utiliza os plugins da pasta .vim/bundle
+" Deve estar carregado em .vim/autoload
+call pathogen#infect()
+syntax on
+filetype plugin indent on
+
+" Colocar comando na Inicialização
+"au VimEnter * LustyFilesystemExplorer 
+"au VimEnter * NERDTree 
+
+" Configurando diretórios
+cd /home/italo/Workspace/suap
+let g:netrw_keepdir=1
+
+" Ajuda terminal https://vimhelp.org/terminal.txt.html
+function CristoReiComandoTerminal(comando)
+  " Nova Aba e abertura de terminal
+  tabnew|terminal
+  " Acha o buffer do terminal
+  let lista_buffers_terminal = filter(map(getbufinfo(), 'v:val.bufnr'), 'getbufvar(v:val, "&buftype") is# "terminal"')
+  let numero_buffer_terminal = lista_buffers_terminal[0]
+  " Envia o comando
+  call term_sendkeys(numero_buffer_terminal, a:comando."\<CR>")
+  " Fecha a janela que já estava aberta
+  wincmd w|bd
+endfunction
+nmap \gs :call CristoReiComandoTerminal('git status')<CR>
+imap \gs <ESC>:call CristoReiComandoTerminal('git status')<CR>
+nmap \gd :call CristoReiComandoTerminal('git diff')<CR>
+imap \gd <ESC>:call CristoReiComandoTerminal('git diff')<CR>
+
+" Exemplo de comando
+"command -nargs=1 MyFunc3 vimgrep <args> ./*/**/*.*
+
+" Configurar o vim-easygrep
+" GrepOptions q
+" Pode colocar para procurar em todo o path setado no cd
+
+" Resolver Warning do LustyFilesystemExplorer
+set hidden
+
+" Vimgrep personalizado
+function CristoReiVimgrep(local,modo)
+  if a:modo != "v"
+    let @l=expand("<cword>")
+  endif
+  if a:local == "p"
+    let @m = split(expand("%p"),"/")[0]
+  else
+    let @m = a:local
+  endif
+  tabnew
+endfunction
+nmap \s :call CristoReiVimgrep("p","")<cr>:vimgrep <c-r>l ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+nmap \d :call CristoReiVimgrep("*","")<cr>:vimgrep <c-r>l ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+imap \s <ESC>:call CristoReiVimgrep("p","")<cr>:vimgrep <c-r>l ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+imap \d <ESC>:call CristoReiVimgrep("*","")<cr>:vimgrep <c-r>l ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+vmap \s "ly<ESC>:call CristoReiVimgrep("p","v")<cr>:vimgrep "<c-r>l" ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+vmap \d "ly<ESC>:call CristoReiVimgrep("*","v")<cr>:vimgrep "<c-r>l" ./<c-r>m/**/*.*<cr>:copen<cr>/<c-r>l<cr>
+
+" Abrir navegador de arquivos
+nmap \n :NERDTree<CR>
+imap \n <ESC>:NERDTree<CR>
+
+" Comentar - Bloco
+function CristoReiComentar()
+  let line_text = getline('.')
+  if line_text[0] == '#'
+    s/^#//
+  else
+    s/^/#/
+  endif
+endfunction
+nmap \m V:call CristoReiComentar()<CR>
+vmap \m :call CristoReiComentar()<CR>
+imap \m <ESC>V:call CristoReiComentar()<CR>i
+" Comentar - Linha Por linha
+imap <F3> <c-c>I#<down>
+nmap <F3> I#<down><c-c>
+imap <F4> <c-c>I<del><down>
+nmap <F4> I<del><down><c-c>
+
+nmap \; :
+imap \; <ESC>:
+
+" Configurações
+set wildignore=*.asta,*.pyc,*.pdf,*.doc,*.xcf,*.png,*.jpg,*.jpeg,*.gif,*.svg,*.mp4,*.mp3,*/cache/*,*/cache_/*,*.ogg,*.tgz,*.webm,*.ttf,*.PDF
+"set incsearch hlsearch wildmenu mat=6 tabstop=4 shiftwidth=4 expandtab cursorline mouse=a autoindent nowrap
+set incsearch hlsearch wildmenu mat=6 tabstop=2 shiftwidth=2 expandtab cursorline mouse=a autoindent nowrap
+syntax on
+
+" Copiar
+vmap <c-c> "+y
+vmap <c-x> "+x
+" Colar
+xmap <c-v> <del>i<ESC>"+p<right>i
+cmap <c-v> <c-r>+
+imap <c-v> <esc>"+p<right>i
+" <c-v> no modo normal e a marcacao por colunas
+nmap <c-v> "+pi
+
+" Seleciona tudo
+nmap <c-a> GVgg
+imap <c-a> <esc>GVgg
+
+" Fazer e Desfazer
+imap <c-z> <esc>ui
+nmap <c-z> u
+imap <c-y> <esc><c-r>i
+nmap <c-y> <c-r>
+
+" Outros
+imap <c-s> <esc>:w<cr>i
+nmap <c-s> :w<cr>
+imap <c-t> <esc>:tabnew<cr>
+nmap <c-t> :tabnew<cr>
+imap <c-d> <esc>:bd<cr>
+nmap <c-d> :bd<cr>
+vmap <bs> <del>
+
+" Busca rapida teclado
+imap <c-left> <esc>Ni
+nmap <c-left> N
+imap <c-right> <esc><right>ni
+nmap <c-right> n
+imap <c-down> <esc>*i
+nmap <c-down> *
+
+" Fechar tudo
+imap <c-q> <esc>:qall<cr>
+nmap <c-q> :qall<cr>
+
+" Selecionar com o teclado
+imap <S-left> <esc>v<left>i
+nmap <S-left> v<left>
+imap <S-right> <esc>v<right>i
+nmap <S-right> v<right>
+imap <S-up> <esc>v<up>i
+nmap <S-up> v<up>
+imap <S-down> <esc>v<down>i
+nmap <S-down> v<down>
+
+" Chamar terminal
+imap <F3> <esc>:terminal<cr>
+nmap <F3> :terminal<cr>
+
+" Buscar
+imap <c-f> <esc>/
+nmap <c-f> /
+
+if has("gui_running")
+  imap <c-o> <esc>:browse confirm e<cr>i
+  nmap <c-o> :browse confirm e<cr>
+else
+  "nmap <c-o> :e\.<cr>
+  nmap <c-o> :LustyFilesystemExplorerFromHere<cr>
+  imap <c-o> <ESC>:LustyFilesystemExplorerFromHere<cr>i
+endif
+
+" GVim
+"imap <c-f> <esc>:promptfind<cr>i
+"nmap <c-f> :promptfind<cr>
+imap <c-h> <esc>:promptrepl<cr>i
+nmap <c-h> :promptrepl<cr>
+amenu icon=/usr/share/icons/hicolor/48x48/apps/xterm-color.png ToolBar.Terminal <esc><c-t>:terminal<cr><c-w>w:bd<cr>
+amenu icon=/usr/share/icons/hicolor/48x48/apps/mate-panel-fish.png ToolBar.LimpaBusca :nohlsearch<cr>
+amenu icon=/usr/share/icons/hicolor/48x48/apps/mate-typing-monitor.png ToolBar.NerdTree :NERDTree<cr>
+amenu icon=/usr/share/icons/hicolor/48x48/apps/mate-typing-monitor.png ToolBar.TabEBrowse :tabnew<cr>:browse confirm e<cr>
+amenu icon=/usr/share/icons/hicolor/48x48/apps/spacefm-48-cube-blue.png ToolBar.QAll :qall<cr>
+
+" VimFileManager
+"nmap <insert> mf<down>|nmap <tab> <c-w>w|let g:netrw_keepdir=0|let g:netrw_localrmdir='rm -rf'
