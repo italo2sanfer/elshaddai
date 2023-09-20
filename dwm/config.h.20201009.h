392b45a944935bec9f3839ebbc16b6e662f25e52
Italo dos Santos Ferreira <iferreira@iff.edu.br>
2023-09-20 10:35:58 -0300

    Ajustes.

--- dwm/config.h.20201009.h
+++ dwm/config.h.20201009.h
@@ -3,11 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -34,14 +29,12 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Google-chrome",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -54,8 +47,8 @@ static const Layout layouts[] = {
 /* Mod1Mask e Alt
  * Mod4Mask e Super_L
  */
-#define MODKEY Mod1Mask
-#define MODKEY2 Mod4Mask
+#define MODKEY2 Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -66,40 +59,29 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
-static const char *midcom[]  = { "xterm", "-e", "mc", NULL };
-/*static const char *rofimenupower[]  = { "rofi", "-show", "p", "-modi", "p:/home/italo/Workspace/elshaddai/rofi-power-menu/rofi-power-menu", "-theme", "Paper", "-width", "20", "-lines", "12", NULL };
-static const char *rofimenu[]  = { "rofi", "-show", "combi", "-combi-modi", "window,drun,ssh", "-modi", "combi", NULL }; */
-static const char *gvim[]  = { "gvim", NULL };
-static const char *chrome[]  = { "google-chrome", NULL };
-static const char *slock[]  = { "slock", NULL };
-static const char *xdotoolfechar[]  = { "xdotool", "key", "alt+F4", NULL };
-static const char *xmenu_main[]  = { "/home/italo/Workspace/elshaddai/xmenu/xmenu_main.sh", NULL };
-/*static const char *xmenu_power[]  = { "/home/italo/Workspace/elshaddai/xmenu/xmenu_power.sh", NULL };*/
-/*static const char *surf[] = { "surf", "duckduckgo.com", NULL };*/
 
-static const Launcher launchers[] = {
-  /* command       name to display */
-	{ xmenu_main,     "[M]"   },
-	{ xdotoolfechar,  "[X]"  },
-	{ chrome,         "Go"   },
-	{ gvim,           "Vim"  },
-	{ termcmd,        "Xt"   },
-};
 
-static const Key keys[] = {
+static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY2,                      XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{0                    , 0x1008ff11 , spawn          , SHCMD ("amixer sset Master 2%-")}, /* Volume - */
+	{0                    , 0x1008ff12 , spawn          , SHCMD ("amixer sset Master toggle")}, /* Volume Mute */
+	{0                    , 0x1008ff13 , spawn          , SHCMD ("amixer sset Master 2%+")}, /* Volume + */
+	{0                    , 0x1008ffb2 , spawn          , SHCMD ("amixer sset Capture toggle")}, /* Mic Mute */
+	{0                    , 0x1008ff59 , spawn          , SHCMD ("$HOME/.xrandr-toggle")}, /* Monitores */
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -123,35 +105,16 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-  /* Atalhos Italo */
-	{ MODKEY2,                      XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY2,                      XK_v,      spawn,          {.v = gvim } },
-	{ MODKEY2,                      XK_c,      spawn,          {.v = chrome } },
-	{ MODKEY2,                      XK_e,      spawn,          {.v = midcom } },
-	{ MODKEY2,                      XK_l,      spawn,          {.v = slock } },
-	{ MODKEY2,                      XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = slock } },
-  { MODKEY|ControlMask,           XK_Right,  view_adjacent,  {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_Left,   view_adjacent,  {.i = -1 } },
-	{ MODKEY,                       XK_F4,     killclient,     {0} },
-	{ MODKEY,                       XK_F1,     spawn,          {.v = xmenu_main } },
-	/*{0                    , 0x1008ff11 , spawn          , SHCMD ("amixer sset Master 2%-")},*/ /* Volume - */
-	/*{0                    , 0x1008ff12 , spawn          , SHCMD ("amixer sset Master toggle")},*/ /* Volume Mute */
-	/*{0                    , 0x1008ff13 , spawn          , SHCMD ("amixer sset Master 2%+")},*/ /* Volume + */
-	/*{0                    , 0x1008ffb2 , spawn          , SHCMD ("amixer sset Capture toggle")},*/ /* Mic Mute */
-	/*{0                    , 0x1008ff59 , spawn          , SHCMD ("$HOME/.xrandr-toggle")},*/ /* Monitores */
-
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static const Button buttons[] = {
+static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	/*{ ClkStatusText,        0,              Button3,        spawn,          {.v = termcmd } },*/
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -159,8 +122,5 @@ static const Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-  /* Atalhos Italo */
-	/*{ ClkStatusText,        0,              Button1,        spawn,          {.v = xmenu_power } },*/
-
 };
 
